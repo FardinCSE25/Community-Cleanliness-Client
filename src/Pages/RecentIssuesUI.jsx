3fbf8fc2b78dd514f1e1733125d60dcc5590fbe3
Fardin Ahmed <fardin.cse25@gmail.com>
2025-11-10 13:54:42 +0600

    some bug fixed

--- src/Pages/RecentIssuesUI.jsx
+++ src/Pages/RecentIssuesUI.jsx
@@ -1,29 +1,30 @@
 import { MapPin } from 'lucide-react';
 import React from 'react';
+import { Link } from 'react-router';
 
-const RecentIssuesUI = ({ issue }) => {
-    if (!issue) return <Loading />
+const RecentIssuesUI = ({issue}) => {
+    
     return (
         <div className="border rounded-xl p-5 shadow-sm bg-white hover:shadow-lg transition">
-            <span className="text-xs px-3 py-1 bg-secondary text-white rounded-full">
-                {issue.category}
-            </span>
+      <span className="text-xs px-3 py-1 bg-secondary text-white rounded-full">
+        {issue.category}
+      </span>
+      
+      <h2 className="text-lg font-semibold mt-3">{issue.title}</h2>
 
-            <h2 className="text-lg font-semibold mt-3">{issue.title}</h2>
+      <p className="text-gray-600 text-sm mt-2 line-clamp-2">
+        {issue.description}
+      </p>
 
-            <p className="text-gray-600 text-sm mt-2 line-clamp-2">
-                {issue.description}
-            </p>
+      <div className="flex items-center gap-1 text-gray-500 text-sm mt-3">
+        <MapPin size={16} />
+        <span>{issue.location}</span>
+      </div>
 
-            <div className="flex items-center gap-1 text-gray-500 text-sm mt-3">
-                <MapPin size={16} />
-                <span>{issue.location}</span>
-            </div>
-
-            <button className="mt-4 w-full bg-primary hover:bg-secondary text-white py-2 rounded-lg transition">
-                See Details
-            </button>
-        </div>
+      <Link to={`/issueDetails/${issue._id}`} className="btn mt-4 w-full bg-primary hover:bg-secondary text-white py-2 rounded-lg transition">
+        See Details
+      </Link>
+    </div>
     );
 };
 
